99357d86ec55f50331edb34dbf41444d45dc51ab
KennySohl <kenny.sohl@gmail.com>
2020-02-20 16:46:39 +0100

    Implementing permission on location

--- app/src/main/java/com/example/mybus/MainActivity.kt
+++ app/src/main/java/com/example/mybus/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.example.mybus
 
+import android.content.pm.PackageManager
+import android.Manifest
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var userLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkLocationPermission()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // User has granted access -> get location
+            getUserLocation()
+        } else {
+            //User has not granted access -> Show error
+            Toast.makeText(this, "We must access your location", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun checkLocationPermission(){
+
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Ask users for permission
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1333)
+            Log.i("TAG", "not granted")
+        }else {
+            // Get user location
+            getUserLocation()
+        }
+
+    }
+
+    private fun getUserLocation() {
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient
+            .lastLocation
+            .addOnSuccessListener {
+                userLocation = it
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Unable to get location", Toast.LENGTH_LONG).show()
+            }
+
     }
 }
